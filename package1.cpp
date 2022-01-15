1e76cc24847e5e8e996ebac53dfa6315dc66f156
Mark <72723296+markl12s@users.noreply.github.com>
2022-01-15 11:10:34 -0600

    untested total_size function

--- package1.cpp
+++ package1.cpp
@@ -1,1 +1,22 @@
-/* Package 1 in C++*/
+#include <iostream>
+
+using namespace std;
+
+// constants
+const MAXIMUM_SIZE = 10;
+const MAXIMUM_WEIGHT = 100;
+
+int main(){
+    // variables
+    int weight = 1;
+    int length = 1;
+    int width = 1;
+    int height = 1;
+
+    int totalSize = total_size(length, width, height);
+    cout << totalSize;
+}
+
+int total_size(length, width, height){totalSize = length * width * height;
+    return totalSize;
+}
