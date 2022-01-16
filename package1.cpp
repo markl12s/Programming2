65c9ff7948a9bbe08b06c36e412b2cab3445b7df
Mark <72723296+markl12s@users.noreply.github.com>
2022-01-15 20:48:37 -0800

    how_much_overload function created
    created function to calculate how much weight it's overloaded by

--- package1.cpp
+++ package1.cpp
@@ -1,3 +1,18 @@
+/*
+* course: BuCS 238 Computer Programming II
+*
+* assignment : 1
+* program : package.cpp
+* programmer : Mark Leard
+*
+* description : compute package size
+*
+*  comments : methods and a function with parameters
+*  style notes : variables camel case, functions snake case, constants upper snake case
+*
+* last modified: 15 Jan 2022
+*/
+
 #include <iostream>
 #include <iomanip>
 
@@ -7,33 +22,62 @@ using namespace std;
 const int MAXIMUM_SIZE = 10;
 const int MAXIMUM_WEIGHT = 100;
 
-int total_size(int length, int width, int height);
+// functions
+void get_input(int& weight, int& length, int& width, int& height);
+int calculate_size(int length, int width, int height);
 bool compare(int total, int target);
+int how_much_overload(int total, int target);
 
 int main() {
     // variables
     int weight = 1;
-    int length = 2;
-    int width = 3;
-    int height = 4;
+    int length = 1;
+    int width = 1;
+    int height = 1;
 
     int totalSize;
     bool tooBig;
     bool tooHeavy;
+    int overWeightTot;
+    int overSizeTot;
 
-    totalSize = total_size(length, width, height);
+    get_input(weight, length, width, height);
+
+    totalSize = calculate_size(length, width, height);
 
     tooBig = compare(totalSize, MAXIMUM_SIZE);
     tooHeavy = compare(weight, MAXIMUM_WEIGHT);
+
+    overWeightTot = how_much_overload(weight, MAXIMUM_WEIGHT);
+    overSizeTot = how_much_overload(totalSize, MAXIMUM_SIZE);
 }
 
-int total_size(int length, int width, int height) {
+void get_input(int& weight, int& length, int& width, int& height) {
+    cout << "Please input the weight" << endl;
+    cin >> weight;
+
+    cout << "Please length the height" << endl;
+    cin >> length;
+
+    cout << "Please input the width" << endl;
+    cin >> width;
+
+    cout << "Please input the height" << endl;
+    cin >> height;
+}
+
+int calculate_size(int length, int width, int height) {
     int finalSize;
-    finalSize = width * length * height;
+    finalSize = length * width * height;
     return finalSize;
 }
 
 bool compare(int total, int target) {
     if (total < target) { return true; }
     else { return false; }
+}
+
+int how_much_overload(int total, int target) {
+    if (total > target) { return target - total; }
+    else { return 0; }
 }
