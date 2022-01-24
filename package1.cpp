a3b3185aa919a1ff653ae5d2dfe0e2ca6c5775f7
Mark <72723296+markl12s@users.noreply.github.com>
2022-01-24 14:16:05 -0600

    finalized code

--- package1.cpp
+++ package1.cpp
@@ -21,11 +21,11 @@ using namespace std;
 
 // constants
 const int MAXIMUM_SIZE = 10;
-const int MAXIMUM_WEIGHT = 100;
+const double MAXIMUM_WEIGHT = 99.9;
 
 // functions
 void get_input(int& weight, int& length, int& width, int& height);
-int calculate_size(int length, int width, int height);
+int calculate_volume(int length, int width, int height);
 bool compare(int total, int target);
 int how_much_overload(int total, int target);
 void print_output(bool tooBig, bool tooHeavy, int overSizeTot, int overWeightTot);
@@ -45,7 +45,7 @@ int main() {
 
     get_input(weight, length, width, height);
 
-    totalSize = calculate_size(length, width, height);
+    totalSize = calculate_volume(length, width, height);
 
     tooBig = compare(totalSize, MAXIMUM_SIZE);
     tooHeavy = compare(weight, MAXIMUM_WEIGHT);
@@ -59,48 +59,104 @@ int main() {
 }
 
 void get_input(int& weight, int& length, int& width, int& height) {
-    cout << "Please input the weight" << endl;
-    cin >> weight;
+    /*
+    * inputs
+    * @weight: the weight of the package
+    * @ length: the length of the package
+    * @width: the width of the package
+    * @height: the height of the package
+    */
 
-    cout << "Please length the height" << endl;
+    cout << "Please input the weight  : ";
+    cin >> weight;
+    cout << endl;
+
+    cout << "Please input the length : ";
     cin >> length;
 
-    cout << "Please input the width" << endl;
+    cout << "Please input the width   : ";
     cin >> width;
 
-    cout << "Please input the height" << endl;
+    cout << "Please input the height  : ";
     cin >> height;
+
+    /*
+    * outputs
+    * @weight: the weight of the package
+    * @ length: the length of the package
+    * @width: the width of the package
+    * @height: the height of the package
+    */
 }
 
-int calculate_size(int length, int width, int height) {
+int calculate_volume(int length, int width, int height) {
+    /*
+    * inputs
+    * @length: the length of the package
+    * @width: the width of the package
+    * @height: the height of the package
+    */
     int finalSize;
     finalSize = length * width * height;
     return finalSize;
+    /*
+    * outputs
+    * @finalSize: the volume of the package
+    */
 }
 
 bool compare(int total, int target) {
+    /*
+    * inputs
+    * @total: the number you are comparing
+    * @target: the number you are comparing to
+    */
+
     if (total >= target) { return true; }
     else { return false; }
+
+    /*
+    * outputs
+    * boolean of whether or not it's bigger
+    */
 }
 
 int how_much_overload(int total, int target) {
+    /*
+    * inputs
+    * @total: the number you are comparing
+    * @target: the number you are comparing to
+    */
+
     if (total > target) { return total - target; }
-    else { return 0; }
+    else { return -42; }
+
+    /* output
+    * integer of how much larger the total is than the target
+    * edgecase: if total <= target returns -42, this shouldn't ever occur in the current form of the project
+    * debug if occurs
+    */
 }
 
 void print_output(bool tooBig, bool tooHeavy, int overSizeTot, int overWeightTot) {
+    /* inputs
+    * @tooBig: whether or not the package is too big
+    * @tooHeavy: whether or not the package is too heavy
+    * @overSizeTot: 
+    */
+
     string message = "";
     int changes = 0;
     string overSizeString = to_string(overSizeTot);
     string overWeightString = to_string(overWeightTot);
-    
-    if ( tooHeavy == true ) {
+
+    if (tooHeavy == true) {
         message = "Package is " + overSizeString + " oversized \n";
         changes += 1;
     }
     else { message = "package is within weight \n"; }
 
-    if ( tooBig == true ) {
+    if (tooBig == true) {
         message += "Package is " + overWeightString + " overweight \n";
         changes += 1;
     }
@@ -111,4 +167,8 @@ void print_output(bool tooBig, bool tooHeavy, int overSizeTot, int overWeightTot
     }
 
     cout << message;
+
+    /*
+    * no outputs should occur
+    */
 }
