07c60438f6897259a0dc3361210d31e259545250
Mark <72723296+markl12s@users.noreply.github.com>
2022-02-14 10:52:38 -0800

    I can't figure this shit out

--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -1,11 +1,45 @@
 /*
-	Programmer    : Bob Harder
+	Programmer    : Mark Leard 
 	Last modified : 26 Jan 22
-
+	
 	sudokustart.cpp
-
-	Demonstrates file processing
+	
+	Demonstrates file processing.
 	Enters a Sudoku puzzle into a two-dimensional array.
+
+	GRADING: 15/50
+
+	Algorithm 0/10
+	None found.
+	Please correct this for the redo.
+
+	Module Interface Table 0/10
+	None found.
+	Please correct this for the redo.
+
+	Program 15/25
+	Program compiles and runs with normal exit.
+	The original Sudoku was not entered.
+	I changed your input text file with the correct input data.
+	Output table is incorrect.
+
+	make sure correct testing data
+
+	Additional comments
+	No loadTable( table ) method found.
+	Table should NOT be a global variable. Pass it as a parameter.
+	Preconditions and Postconditions are statements that are true or false.
+	Correct and redo.
+
+	create a loadTable method
+
+	Test Plan 0/5
+	None found.
+	Please correct this for the redo.
+
+	make sure to turn in everything
+
+	Issues:
 */
 
 #include <iostream>
@@ -16,15 +50,20 @@ using namespace std;
 
 const string INFILENAME = "sudokutest.txt";
 
-int main()
+void findAndReplace(char target, int changeNumber);
 
+
+int main()
 {
+	char[]  table[9][9];            // sudoku table
+	int[]   finalTable[9][9];
+
+
 	// constants and variables
 	ifstream inFile;                 // input file variable
 	string   buf;                    // input buffer
 
 	char     value;                  // value read from file
-	char     table[9][9];            // sudoku table
 
 	int      linecount = 0;
 
@@ -38,8 +77,10 @@ int main()
 	}
 
 	// process the file
-// 	while (!inFile.eof())
-	for (int row = 0; row < 9; row++)            // rows
+	// 	while (!inFile.eof())
+
+
+  	for (int row = 0; row < 9; row++)            // rows
 	{
 		for (int col = 0; col < 9; col++)        // columns
 		{
@@ -82,7 +123,7 @@ int main()
 	}
 
 
-	cout << endl;
+    cout << endl;
 	// food for thought
 	// make pointer p point to the same address space as table
 	char* p = *table;
@@ -91,6 +132,42 @@ int main()
 		cout << p[i];
 	cout << endl;
 
+	findAndReplace('7', 8, table, finalTable);
+	findAndReplace('8', 9, table, finalTable);
+	findAndReplace('9', 7, table, finalTable);
+	cout << endl << endl << endl;
 
-	return(0);
+	for (int i = 0; i < 9; i++) {
+		cout << endl;
+		for (int a = 0; a < 9; a++)
+			cout << finalTable[i][a] << " ";
+	}
+	return( 0 );
 }
+
+// include table as param and target
+void findAndReplace(char target, int changeNumber, char[] table, int[] finalTable)
+/*
+* Pre :  a 9x9 grid of char 1-9
+* Post: The completed board
+*/
+{
+	for (int i = 0; i < 9; i++)
+	{
+		for (int a = 0; a < 9; a++)
+		{
+			if (table[i][a] == target)
+			{
+			 	finalTable[i][a] = (char)changeNumber;
+			}
+			else
+			{
+				finalTable[i][a] = ((int)table[i][a]);
+			}
+			
+			cout << finalTable[i][a];
+		}
+	}
+}
+
+
