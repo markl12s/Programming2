40d4a6881535a06e8ec68988443931bb206f3af6
Mark <72723296+markl12s@users.noreply.github.com>
2022-02-21 14:03:41 -0800

    Create TwoNum.cpp

---
+++ TwoNums/TwoNum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include "TwoNum.h"
+
+using namespace std;
+
+int main() {
+	// testing default constructor
+	TwoNum nums();
+
+	// testing get numbers
+	// nums.GetNumbers();
+
+	// testing normal constructor
+	// TwoNum nums1(42, 42);
+	// nums1.GetNumbers;
+
+	return 0;
+}
